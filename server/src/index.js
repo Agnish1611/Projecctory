39b8162532ea2de78052f32c5e06d8a90f9eb487
Agnish1611 <sukla.baruipur2@gmail.com>
2024-07-13 03:05:30 +0530

    Completed fetching labels into sidebar

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from 'cors';
 
 import connect from "./config/db-config.js";
 import { PORT } from "./config/env-config.js";
@@ -7,6 +8,8 @@ import { PORT } from "./config/env-config.js";
 import apiRoutes from './routes/index.js';
 
 const app = express();
+
+app.use(cors({origin: 'http://localhost:5173'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
